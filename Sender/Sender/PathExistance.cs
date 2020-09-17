cee25b27282412ae7451def91160207f3483f76d
patreshubham94 <68467015+patreshubham94@users.noreply.github.com>
2020-09-17 11:46:30 +0530

    Updating Sender

---
+++ Sender/Sender/PathExistance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sender
+{
+    public class PathExistance
+    {
+        public bool checkFileExistorNot(string path)
+        {
+            if (File.Exists(path))
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Sender/Sender/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Data;
+
+namespace Sender
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = @"C:\Users\320103928\Case_Study1\reviewreport.csv";
+            //Necessary to put @ symbol at start of path, 
+            //else it gives "System.ArgumentException: 'Illegal characters in path.'"
+            PathExistance exist = new PathExistance();
+            bool checkPath = exist.checkFileExistorNot(path);
+
+            if (checkPath)
+            {
+                string[] lines = System.IO.File.ReadAllLines(path);
+
+                csvPrinter printCSV = new csvPrinter();
+                printCSV.printTheCSVFile(lines);
+            }
+            else
+            {
+                Console.WriteLine("ALERT..! Check file available or not..!");
+            }
+
+        }
+    }
+}
