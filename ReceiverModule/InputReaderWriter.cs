b7c9ed768ba400280c0eacdaff0f7aa766213a93
Sharma <Dikshit.Sharma@philips.com>
2020-09-21 16:58:10 +0530

    Optimized ReceiverModule Code and added unit tests

--- ReceiverModule/InputReaderWriter.cs
+++ ReceiverModule/InputReaderWriter.cs
@@ -4,61 +4,74 @@ using System.IO;
 
 namespace ReceiverModule
 {
-    public class InputReaderWriter
+    public abstract class InputReader
     {
-        public class ConsoleInputReader
+        public abstract List<string> ReadInput();
+    }
+
+    public abstract class OutputGenerator
+    {
+        public abstract void CsvGenerator(Dictionary<string, int> processedData, bool noInputDetected);
+    }
+
+    public class ConsoleInputReader : InputReader
+    {
+        public override List<string> ReadInput()
         {
-            public static List<string> ReadInput()
+            var rawData = new List<string>();
+            string comment;
+            while ((comment = Convert.ToString(Console.ReadLine())) != "EOF")
             {
-                var rawData = new List<string>();
-                string comment;
-                while ((comment = Convert.ToString(Console.ReadLine())) != "EOF")
+                rawData.Add(comment);
+            }
+            return rawData;
+        }
+    }
+
+    public class OutputWriter : OutputGenerator
+    {
+        public override void CsvGenerator(Dictionary<string, int> processedData, bool noInputDetected)
+        {
+            if (noInputDetected)
+            {
+                var outputFile = new StreamWriter("output.csv");
+                outputFile.WriteLine("No Data");
+                outputFile.Close();
+            }
+            else
+            {
+                var outputFile = new StreamWriter("output.csv");
+                foreach (var wordCount in processedData)
                 {
-                    rawData.Add(comment);
+                    outputFile.WriteLine(wordCount.Key + "," + wordCount.Value);
+                    Console.WriteLine(wordCount.Key + " " + wordCount.Value);
                 }
-                return rawData;
+                Console.ReadLine();
+                outputFile.Close();
             }
         }
 
-        public class OutputWriter
-        {
-            public static void CsvGenerator(Dictionary<string, int> processedData, bool noInputDetected)
-            {
-                if (noInputDetected)
-                {
-                    var outputFile = new StreamWriter("output.csv");
-                    outputFile.WriteLine("No Data");
-                    outputFile.Close();
-                }
-                else
-                {
-                    var outputFile = new StreamWriter("output.csv");
-                    foreach (var wordCount in processedData)
-                    {
-                        outputFile.WriteLine(wordCount.Key + "," + wordCount.Value);
-                        Console.WriteLine(wordCount.Key + " " + wordCount.Value);
-                    }
-                    Console.ReadLine();
-                    outputFile.Close();
-                }
-            }
-
-        }
+    }
+    public class MainMethod
+    {
         private static void Main(string[] args)
         {
             Console.WriteLine("Sit Back and Relax and let me do my thing .......");
-            var inputData = ConsoleInputReader.ReadInput();
+            var getInput = new ConsoleInputReader();
+            var writeOutput = new OutputWriter();
+
+            var inputData = getInput.ReadInput();
             if (inputData.Count == 0)
             {
                 var dic = new Dictionary<string, int>();
-                OutputWriter.CsvGenerator(dic, true);
+                writeOutput.CsvGenerator(dic, true);
             }
             else
             {
                 InputMining mineInput = new InputMining();
                 var wordCount = mineInput.ProcessInput(inputData);
                 //wordCount = removeStopWords.removeStopWorsFromProcessedData(wordCount);
-                OutputWriter.CsvGenerator(wordCount, false);
+                writeOutput.CsvGenerator(wordCount, false);
 
             }
         }
